b750bae62b7c4d197e05e3f06c5a8ea5705a727d
jsk-4970 <aburiaburisamon@gmail.com>
2025-11-18 15:54:46 +0900

    by1117

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,16 +6,30 @@
 /*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 16:04:04 by jyamada           #+#    #+#             */
-/*   Updated: 2025/11/15 16:08:34 by jyamada          ###   ########.fr       */
+/*   Updated: 2025/11/18 15:54:29 by aburi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#ifndef BUFSIZE
+# define BUFSIZE 42
+#endif
+
+typedef struct s_list
+{
+    char            *contents;
+    size_t          len;
+    struct s_list   *next;
+}                   t_list;
 
 char	*get_next_line(int fd);
+int     ft_getc(int fd);
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,10 +6,43 @@
 /*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 16:03:04 by jyamada           #+#    #+#             */
-/*   Updated: 2025/11/15 16:53:42 by jyamada          ###   ########.fr       */
+/*   Updated: 2025/11/17 13:48:08 by aburi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+int	ft_getc(int fd)
+{
+	static char buf[BUFSIZE];
+	static char *bufp;
+	static int n;
+
+	if (n == 0)
+	{
+		n = read(fd, buf, sizeof(0));
+		bufp = buf;
+	}
+	if (--n >= 0)
+		return ((unsigned char)*bufp++);
+	else
+		return (EOF);
+}
+
+//int main(void)
+//{
+//	int fd;
+//	int c;
+//
+//	fd = open("text.txt", O_RDONLY);
+//	printf("fd:%d\n", fd);
+//	while (1)
+//	{
+//		c = ft_getc(fd);
+//		if (c == EOF)
+//			break;
+//		printf("%c", c);
+//	}
+//	close (fd);
+//	return (0);
+//}
