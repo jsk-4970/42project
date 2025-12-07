524d88af8a32939ed9744d05b1c8d90d44ab02a1
jsk-4970 <aburiaburisamon@gmail.com>
2025-12-07 20:38:00 +0900

    by1207

--- get_next_line_array/get_next_line.c
+++ get_next_line_array/get_next_line.c
@@ -3,29 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: aburi <aburi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 16:04:08 by jyamada           #+#    #+#             */
-/*   Updated: 2025/11/30 19:05:10 by jyamada          ###   ########.fr       */
+/*   Updated: 2025/12/06 20:54:20 by aburi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <sys/types.h>
+#include <stdlib.h>
 
-//size_t	ft_strlen(const char *str)
-//
-//{
-//	size_t	len;
-//
-//	if (str == NULL)
-//		return (0);
-//	len = 0;
-//	while (str[len])
-//		len++;
-//	return (len);
-//}
+size_t	ft_strlen(const char *str)
+
+{
+	size_t	len;
+
+	if (str == NULL)
+		return (0);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -33,8 +32,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dlen;
 	size_t	n;
 
-	slen = BUFSIZE;
-	dlen = dstsize;
+	slen = ft_strlen(src);
+	dlen = ft_strlen(dst);
 	n = 0;
 	if (dstsize <= dlen)
 		return (dstsize + slen);
@@ -47,42 +46,66 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	return (dlen + slen);
 }
 
+
+//void	*ft_memcpy(void *dest, const void *src, size_t n)
+//{
+//	size_t			i;
+//	unsigned char	*udest;
+//	unsigned char	*usrc;
+//
+//	i = 0;
+//	udest = (unsigned char *)dest;
+//	usrc = (unsigned char *)src;
+//	if (n == 0 || (udest == NULL && usrc == NULL))
+//		return (udest);
+//	while (i < n)
+//	{
+//		udest[i] = usrc[i];
+//		i++;
+//	}
+//	return (udest);
+//}
+
 //get file and write the contents to buf until end of file
 //if read() fails, return NULL;
-char	*ft_getfile(int fd,  char *tmp_stash)
+//tmp_stash is actually (static char *)stash
+//new_stash is 
+char	*ft_getfile(int fd,  char *stash)
 {
 	ssize_t		ret_read;
 	char		*buf;
+	char		*tmp_stash;
 	int			i;
 
-	buf = NULL;
 	i = 0;
 	ret_read = 1;
 	while (ret_read != 0)
 	{
-		free(buf);
-		buf = malloc(BUFSIZE);
+		buf = malloc(BUFSIZE + 1);
 		ret_read = read(fd, buf, BUFSIZE);
-//		printf("index:%d", i);
-//		write(1, buf, BUFSIZE);
-//		printf("\n");
 		if (ret_read == -1)
 			return (NULL);
-		free(tmp_stash);
+		buf[ret_read] = '\0';
 		i++;
 		tmp_stash = malloc(BUFSIZE * i + 1);
+		tmp_stash[0] = '\0';
+		if (stash != NULL)
+		{
+			ft_strlcat(tmp_stash, stash, BUFSIZE * i + 1);
+			free(stash);
+		}
 		ft_strlcat(tmp_stash, buf, BUFSIZE * i + 1);
-		write(1, tmp_stash, BUFSIZE * i);
+		stash = tmp_stash;
+		free(buf);
 	}
-	free(buf);
-	return (tmp_stash);
+	return (stash);
 }
 
-//char	*ft_extract_line(char *tmp_stash)
-//{
-//	char	*line;
-//	return (line);
-//}
+// char	*ft_extract_line(char *tmp_stash)
+// {
+// 	char	*line;
+// 	return (line);
+// }
 
 //char	*ft_clean_stash(char *tmp_stash)
 //{
@@ -94,7 +117,8 @@ char	*ft_getfile(int fd,  char *tmp_stash)
 //	static char	*stash;
 //	char		*line;
 //
-//	stash = ft_getfile(fd, stash);
+//	if (stash == NULL)
+//		stash = ft_getfile(fd, stash);
 //	line = ft_extract_line(stash);
 //	stash = ft_clean_stash(stash);
 //	return (line);
--- get_next_line_array/get_next_line.h
+++ get_next_line_array/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 16:06:00 by jyamada           #+#    #+#             */
-/*   Updated: 2025/11/30 16:29:12 by jyamada          ###   ########.fr       */
+/*   Updated: 2025/12/06 16:23:04 by aburi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/types.h>
 
 #ifndef BUFSIZE
 # define BUFSIZE 42
@@ -24,6 +25,7 @@
 
 char	*ft_getfile(int fd, char *buf);
 char	*ft_extract_line(char *buf);
+char    *ft_clean_stash(char *tmp_stash);
 char	*get_next_line(int fd);
 
 #endif
