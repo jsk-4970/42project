fc1ff8765b7db8189140810ef5badc939b27cd55
jsk-4970 <210462724+jsk-4970@users.noreply.github.com>
2025-12-23 15:46:11 +0900

    add swap

---
+++ push_swap/include/push_swap.h
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 13:41:50 by jyamada           #+#    #+#             */
+/*   Updated: 2025/12/23 15:16:11 by jyamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <inttypes.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//push_swap
+
+
+//libft
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+//libftprintf
+
+int		ftp_strlen(const char *str);
+int		ftp_strlcpy(char *dest, const char *src, size_t dstsize);
+int		ftp_putchar(char c);
+int		ftp_putstr(char *s);
+int		ftp_putnbr(long n);
+int		ftp_read_next(const char *format, va_list args);
+int		ft_printf(const char *format, ...);
+int		put_c(int c);
+int		put_s(char *s);
+int		put_p(uintptr_t ptr);
+int		put_d(long d);
+int		put_u(unsigned long u);
+int		put_x(unsigned long x, char c);
+
+#endif
---
+++ push_swap/src/push_swap.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 13:20:18 by jyamada           #+#    #+#             */
+/*   Updated: 2025/12/23 15:45:08 by jyamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+//if str is not int, return 0
+int		is_valid_int(char *str)
+{
+	int		len;
+	int		is_minus;
+
+	is_minus = 0;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			is_minus == 1;
+		str++;
+	}
+	if (!is_all_digits(str))
+		return (0);
+	while (*str == '0' && *(str + 1))
+		str++;
+	len = ft_stelen(str);
+	if (len > 10)
+		return (0);
+	if (is_minus)
+		return (ft_strncmp(str, "2147483648", len) <= 0);
+	else
+		return (ft_strncmp(str, "2147483648", len) <= 0);
+	return (0);
+}
+
+char	*validate_args(char **av)
+{
+	char	*ret;
+
+
+}
+
+int	main(int ac, char **av)
+{
+	char	*args;
+
+	if (ac <= 1)
+		return (0);
+	if (ac == 2)
+		args = split_and_validate(av[1]);
+	else
+		args = validate_args(ac - 1, &av[1]);
+	if (!args)
+		return (1, write(2, "Error\n", 6));
+	push_swap(args);
+	free_args(args);
+	return (0);
+}
