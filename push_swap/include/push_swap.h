66e9b3c763db52e065d65bba6d13ea5892371442
jsk-4970 <aburiaburisamon@gmail.com>
2025-12-26 16:45:42 +0900

    define structure

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -3,79 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: aburi <aburi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 13:41:50 by jyamada           #+#    #+#             */
-/*   Updated: 2025/12/23 15:16:11 by jyamada          ###   ########.fr       */
+/*   Updated: 2025/12/26 15:26:05 by aburi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <limits.h>
+# include <stdarg.h>
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <inttypes.h>
-# include <limits.h>
-# include <stdarg.h>
-# include <stdlib.h>
-# include <unistd.h>
 
-//push_swap
+typedef struct s_stack
+{
+	int	*data;
+	int	size;
+	int	capacity;
+}	t_stack;
 
+/* main.c */
+int		is_valid_int(char *str);
+char	*validate_args(char **av);
 
-//libft
+/* parse.c */
+int		is_all_digits(char *str);
+char	*split_and_validate(char *str);
+void	free_args(char *args);
 
-int		ft_isalpha(int c);
+/* sort.c */
+void	push_swap(char *args);
+
+/* operation.c - swap */
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+
+/* operation.c - push */
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+
+/* operation.c - rotate */
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+
+/* operation.c - reverse rotate */
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
+
+/* utils.c */
+void	error_exit(void);
+
 int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strdup(const char *s);
+char	**ft_split(char const *s, char c);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-
-//libftprintf
-
-int		ftp_strlen(const char *str);
-int		ftp_strlcpy(char *dest, const char *src, size_t dstsize);
-int		ftp_putchar(char c);
-int		ftp_putstr(char *s);
-int		ftp_putnbr(long n);
-int		ftp_read_next(const char *format, va_list args);
-int		ft_printf(const char *format, ...);
-int		put_c(int c);
-int		put_s(char *s);
-int		put_p(uintptr_t ptr);
-int		put_d(long d);
-int		put_u(unsigned long u);
-int		put_x(unsigned long x, char c);
 
 #endif
