451643fae57d40dfb0b9d4782ff6df219ab679e7
jsk-4970 <210462724+jsk-4970@users.noreply.github.com>
2026-01-23 15:20:52 +0900

    refactor

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aburi <aburi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 13:41:50 by jyamada           #+#    #+#             */
-/*   Updated: 2026/01/23 13:26:07 by jyamada          ###   ########.fr       */
+/*   Updated: 2026/01/23 15:19:34 by jyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,9 @@ typedef struct s_stack
 }	t_stack;
 
 /* main.c */
-t_stack	stack_new(int value);
-int		is_valid_int(char *str);
-char	*validate_args(char **av);
 t_stack	*find_last_stack(t_stack *list);
 
 /* parse.c */
-int		is_all_digits(char *str);
-char	*split_and_validate(char *str);
 
 /* sort.c */
 void	push_swap(t_stack **a, t_stack **b);
@@ -65,18 +60,14 @@ void	rrb(t_stack **b);
 void	rrr(t_stack **a, t_stack **b);
 
 /* utils.c */
-void	error_exit(void);
 
 void	free_args(char **args, int flag_to_free);
 void	free_stack(t_stack *a);
 int		ft_isdigit(int c);
 size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 char	**ft_split(char const *s, char c);
 long	ft_atol(const char *nptr);
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
 
