33b86c7faafdbc473839cb8f901d5a395f2abf5d
jsk-4970 <210462724+jsk-4970@users.noreply.github.com>
2026-01-23 14:15:38 +0900

    fix the logic of sort, the condition of hwile

--- push_swap/src/sort.c
+++ push_swap/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: jyamada <jyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:36:34 by jyamada           #+#    #+#             */
-/*   Updated: 2026/01/23 13:38:13 by jyamada          ###   ########.fr       */
+/*   Updated: 2026/01/23 14:14:19 by jyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,17 @@ void	kick_back(t_stack **a, t_stack **b)
 void	push_swap(t_stack **a, t_stack **b)
 {
 	int	size;
+	int	max_rank;
+	int	max_bits;
 	int	i;
 
-	size = re_rank(a) + 1;
+	max_rank = re_rank(a);
+	size = max_rank + 1;
+	max_bits = 0;
+	while ((max_rank >> max_bits) != 0)
+		max_bits++;
 	i = 0;
-	while (i < size)
+	while (i < max_bits)
 	{
 		separate(a, b, i, size);
 		kick_back(a, b);
