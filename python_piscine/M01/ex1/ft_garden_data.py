f4557791d2a99e90950b91d0dd452a5b8a71913f
jsk-4970 <aburiaburisamon@gmail.com>
2026-02-11 16:33:49 +0900

    fix the ex1

--- python_piscine/M01/ex1/ft_garden_data.py
+++ python_piscine/M01/ex1/ft_garden_data.py
@@ -8,14 +8,14 @@ class Plant:
 	
 
 def ft_garden_data():
-	rose = Plant(Rose, 25, 30)
-	sunflower = Plant(Sunflower, 80, 45)
-	cactus = Plant(Cactus, 15, 120)
+	rose = Plant("Rose", 25, 30)
+	sunflower = Plant("Sunflower", 80, 45)
+	cactus = Plant("Cactus", 15, 120)
 
 	print("=== Garden Plant Registry ==")
-	print(get_info(rose))
-	print(get_info(sunflower))
-	print(get_info(cactus))
+	print(rose.get_info())
+	print(sunflower.get_info())
+	print(cactus.get_info())
 
-if __name__ == "main":
+if __name__ == "__main__":
 	ft_garden_data()
